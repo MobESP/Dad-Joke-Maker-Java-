ca6d84d793dd8043e3c46f6962d9b3abbd3222c9
MobESP <51514619+MobESP@users.noreply.github.com>
2021-04-17 22:18:33 -0400

    Created the main script page

---
+++ Script.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.random;
+
+
+public class DadJokeMaker {
+
+    public static void main(String[] args) {
+
+        int lettuce = "lettuce!";
+        int chug_jug = "chug jug";
+        int boo = "boo";
+        int rap = "rap";
+
+        Scanner reader = new Scanner(System.in);
+
+        Random rand = new Random();
+
+        system.out.print("Random integers for number output; disregard "+rand_Int1);
+
+        system.out.printLn("Knock Knock (Psst! your supposed to say 'Whose there' :D!");
+
+        if rand_Int1=1;
+            system.out.print(lettuce);
+        
+        if rand_Int1=2;
+            system.out.print(chug_jug);
+        
+        if rand_Int1=3;
+            system.out.print(boo);
+
+        if rand_Int1=4;
+            system.out.print(rap);
+
+    }
+}
